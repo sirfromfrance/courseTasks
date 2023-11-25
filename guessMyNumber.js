4d1315ff69e459244614acb2daaf6329b47cc566
sirfromfrance <107402489+sirfromfrance@users.noreply.github.com>
2023-11-25 12:59:22 +0200

    Update guessMyNumber.js

--- guessMyNumber.js
+++ guessMyNumber.js
@@ -38,8 +38,8 @@ document.querySelector(".check").addEventListener("click", function () {
 
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
-  document.querySelector(".score").textContent = score;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".score").textContent = score;
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".guess").value = "";
   displaySecretNum("?");
