c6c73560b1da5af82a7dcd89ab578f78fd176334
sirfromfrance <107402489+sirfromfrance@users.noreply.github.com>
2024-02-07 11:17:10 +0200

    Update course7.js

--- pigGame/course7.js
+++ pigGame/course7.js
@@ -65,7 +65,7 @@ btnHold.addEventListener("click", function () {
     scores[activePlayer] = scores[activePlayer] + currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] > 20) {
+    if (scores[activePlayer] > 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
